09e0761b242bd095d5a0b3d936c97948172b6964
Galkin Alexey Andreevich <97546730+GalkinAlexeyAndreevich@users.noreply.github.com>
2023-04-19 11:29:17 +0300

    Функция коллизии

--- js игра(без canvas)/main.js
+++ js игра(без canvas)/main.js
@@ -1,16 +1,16 @@
 
-class Player{
+class Player1{
     constructor(){
-        this.width = 100,
-        this.height = 20,
+        this.width = 20,
+        this.height = 100,
         this.x = 300,
         this.y = 300,
-        this.maxX = 400,
+        this.maxX = 300,
         this.maxY = 400,
-        this.minX = 200,
+        this.minX = 300,
         this.minY = 200,
         this.colorObj = "red",
-        this.element = document.querySelector(".player")
+        this.element = document.querySelector(".player1")
     }
     onClick(e){
         switch(e.code){
@@ -20,12 +20,6 @@ class Player{
                 break
             case "ArrowDown":
                 this.y = this.y + 5
-                break
-            case "ArrowLeft":
-                this.x = this.x - 5
-                break
-            case "ArrowRight":
-                player.x = player.x + 5
                 break
         }
     }
@@ -45,22 +39,35 @@ class Player{
         // colorPlayer
     }
 
-
-
-
+}
+class Player2 extends Player1{
+    constructor(){
+        super()
+        this.width = 20,
+        this.height = 100,
+        this.x = 600,
+        this.y = 300,
+        this.maxX = 600,
+        this.maxY = 400,
+        this.minX = 600,
+        this.minY = 200,
+        this.colorObj = "red",
+        this.element = document.querySelector(".player2") 
+    }
 }
 
 class Ball{
     constructor(){
         this.width = 20,
         this.height = 20,
-        this.x = 350,
-        this.y = 150,
+        this.x = 330,
+        this.y = 325,
         this.maxX = 400,
         this.maxY = 400,
         this.minX = 200,
         this.minY = 200,
         this.colorObj = "green",
+        this.velocity = -5
         this.element = document.querySelector(".ball")
     }
     updateLocation(){
@@ -77,34 +84,67 @@ class Ball{
         this.element.style.backgroundColor = this.colorObj
         // colorPlayer
     }
-    ballDown(){
+    ballLeft(){
         console.log(this.y);
-        this.y = this.y-5
+        this.x = this.x -5
+        // console.log(this.y);
+        this.updateLocation()
+    }
+    ballRight(){
+        console.log(this.y);
+        this.x = this.x+5
         // console.log(this.y);
         this.updateLocation()
     }
     
 }
 
-const player = new Player()
-const ball = new Ball()
+let player1 = new Player1()
+let player2 = new Player2()
+let ball = new Ball()
 
 
 setInterval(()=>{
-    ball.ballDown()
+    if(collizia(player1,ball)){
+        // ball.ballLeft()
+        ball.ballRight()
+        console.log(12345);
+        return
+    }
+    // else if(collizia(player1,ball) ){
+    //     ball.ballRight()
+    //     console.log(1234);
+    //     return
+    // }
+    ball.ballLeft()
+    
 },500)
 
-player.updateLocation()
+player1.updateLocation()
+player2.updateLocation()
 ball.updateLocation()
 
 
 document.body.addEventListener("keydown",(e)=>{
-    player.onClick(e)
-    player.updateLocation()
+    player1.onClick(e)
+    player1.updateLocation()
+    player2.onClick(e)
+    player2.updateLocation()
+    // collizia(player,ball)
         
 })
 
+function collizia(player,ball){
+    if(player.x-player.width  <= ball.x && player.x>= ball.x-ball.width){
+        console.log("collide");
+        return true
+    }
+    return false
+
+}
+// collizia(player,ball)
 
 
-player.updateSize()
+player1.updateSize()
+player2.updateSize()
 ball.updateSize()
